166e1188f0b3003c27fd4728e606eca79a731a15
James Alt <james.alt@me.com>
2016-12-02 17:05:49 -0600

    Initial Push

---
+++ ReactiveDemo.Core/ViewModels/LoginViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace ReactiveDemo.Core
+{
+	public class LoginViewModel
+		: ReactiveObject
+	{
+		public LoginViewModel()
+		{
+			var canLogin = this.WhenAnyValue(
+				p => p.Username,
+				p => p.Password,
+				(usernameValue, passwordValue) =>
+				{
+					return !string.IsNullOrWhiteSpace(usernameValue)
+								  && !string.IsNullOrWhiteSpace(passwordValue);
+				});
+
+			Login = ReactiveCommand.CreateFromTask(
+				async () =>
+				{
+					await Task.Delay(4000).ConfigureAwait(false);
+				}, 
+				canLogin);
+
+			Login.IsExecuting.ToProperty(this, p => p.IsLoading, out isLoading);
+		}
+
+		#region Commands
+
+		public ReactiveCommand Login { get; protected set; }
+
+		#endregion
+
+		#region Properties
+
+		string username;
+		public string Username
+		{
+			get { return username; }
+			set { this.RaiseAndSetIfChanged(ref username, value); }
+		}
+
+		string password;
+		public string Password
+		{
+			get { return password; }
+			set { this.RaiseAndSetIfChanged(ref password, value); }
+		}
+
+		readonly ObservableAsPropertyHelper<bool> isLoading;
+		public bool IsLoading
+		{
+			get { return isLoading.Value; }
+		}
+
+		#endregion
+	}
+}
---
+++ ReactiveDemo/Pages/LoginPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ReactiveUI;
+using ReactiveDemo.Core;
+using Xamarin.Forms;
+using System;
+using System.Reactive.Linq;
+
+namespace ReactiveDemo
+{
+	public partial class LoginPage 
+		: ContentPage, IViewFor<LoginViewModel>	
+	{
+		public LoginPage()
+		{
+			InitializeComponent();
+			ViewModel = new LoginViewModel();
+
+			this.Bind(ViewModel, vm => vm.Username, v => v.Username.Text);
+			this.Bind(ViewModel, vm => vm.Password, v => v.Password.Text);
+			this.BindCommand(ViewModel, vm => vm.Login, v => v.Login);
+
+			this.WhenAnyValue(x => x.ViewModel.IsLoading)
+				.Subscribe(isBusy =>
+					{
+						Username.IsEnabled = !isBusy;
+						Password.IsEnabled = !isBusy;
+						LoadingIndicator.IsRunning = isBusy;
+					});
+		}
+
+		public static readonly BindableProperty ViewModelProperty =
+			BindableProperty.Create(
+				nameof(ViewModel),
+				typeof(LoginViewModel),
+				typeof(LoginPage),
+				null, 
+				BindingMode.OneWay);
+
+		public LoginViewModel ViewModel
+		{
+			get { return (LoginViewModel)GetValue(ViewModelProperty); }
+			set { SetValue(ViewModelProperty, value); }
+		}
+
+		object IViewFor.ViewModel
+		{
+			get { return ViewModel; }
+			set { ViewModel = (LoginViewModel)value; }
+		}
+	}
+}
